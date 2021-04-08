65d53066582b2306b58369be28666178e4df7efa
ajpkim swift3 <alexkim345@gmail.com>
2021-04-07 22:42:40 -0400

    dropped obsolete voting app

--- voting/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Election, ElectionProposal, Score, Proposal, Vote
-
-admin.site.register([Election, ElectionProposal, Score, Proposal, Vote])
--- voting/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class VotingConfig(AppConfig):
-    name = 'voting'
-
-    def ready(self):
-        import voting.signals
